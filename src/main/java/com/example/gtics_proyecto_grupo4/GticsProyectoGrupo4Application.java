841194ce81a76678b9ddff7a779d511a7be80c88
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2023-04-18 20:07:19 -0500

    Initial commit

---
+++ src/main/java/com/example/gtics_proyecto_grupo4/GticsProyectoGrupo4Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_proyecto_grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsProyectoGrupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsProyectoGrupo4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics_proyecto_grupo4/GticsProyectoGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_proyecto_grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsProyectoGrupo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
