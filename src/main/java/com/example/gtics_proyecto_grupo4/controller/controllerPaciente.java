5b7d6bb68a5fbf36d8e5d53eba0cfe00090705b2
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2023-04-18 23:56:09 -0500

    primeras carpetas

---
+++ src/main/java/com/example/gtics_proyecto_grupo4/controller/controllerPaciente.java
@@ -0,0 +1,51 @@
+package com.example.gtics_proyecto_grupo4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/Paciente")
+public class controllerPaciente {
+
+    @RequestMapping(value={"","/"},method = RequestMethod.GET)
+    public String vistBase(){
+        return "paciente/dashboardVistaPrincipalPaciente";
+    }
+
+    @RequestMapping(value={"/altern"},method = RequestMethod.GET)
+    public String vistaAlterna(){
+
+        return "paciente/Paciente";
+    }
+
+    @RequestMapping(value={"/Cuestionario"},method = RequestMethod.GET)
+    public String vistaPacienteCuestionarios(){
+
+        return "paciente/PacienteCuestionario";
+    }
+
+    @RequestMapping(value={"/Citas"},method = RequestMethod.GET)
+    public String vistaCitas(){
+
+        return "paciente/PacienteCitas";
+    }
+
+    @RequestMapping(value={"/Agendar"},method = RequestMethod.GET)
+    public String vistaAgendarCitas(){
+
+        return "paciente/PacienteAgendarCita";
+    }
+
+    @RequestMapping(value={"/Perfil"},method = RequestMethod.GET)
+    public String vistaPerfil(){
+
+        return "paciente/PacientePerfil";
+    }
+
+    @RequestMapping(value={"/HistorialCitas"},method = RequestMethod.GET)
+    public String vistaHistorialCitas(){
+
+        return "paciente/PacienteHistorialCitas";
+    }
+}
